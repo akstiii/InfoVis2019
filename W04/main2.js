00d3fba939046733cebc29c92eaf8abcc221d0db
HOSIO AKU MATTI VILJAMI <191xi03x@istc-a-071.epc.center.kobe-u.ac.jp>
2019-04-16 14:50:44 +0900

    W04: Task 2 tune

--- W04/main2.js
+++ W04/main2.js
@@ -11,14 +11,14 @@ function main()
     var far = 1000;
     var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.set(0, 0, 5);
-    scene.add( camera );
+    scene.add(camera);
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(width, height);
     document.body.appendChild(renderer.domElement);
 
     var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshLambertMaterial({ color: 0xffffff });
+    var material = new THREE.MeshLambertMaterial({color: 0xffffff});
     var cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
