ab8fca85221b0f8297550620e3cbdb2c0d558fa3
AukustiE <aukustie@live.com>
2019-05-08 03:37:42 +0900

    W05: Task 1 & 2

--- W05/task1.js
+++ W05/task1.js
@@ -10,86 +10,79 @@ function main()
     var near = 1;
     var far = 1000;
     var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-    camera.position.set( 0, 0, 5 );
+    camera.position.set( 0, 0, 10 );
     scene.add( camera );
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize( width, height );
     document.body.appendChild( renderer.domElement );
 
-
-
-    var material = new THREE.MeshBasicMaterial();
-    material.vertexColors = THREE.FaceColors;
-    geometry.faces[0].color = new THREE.Color(1, 0, 0);
-
-    //var material = new THREE.MeshBasicMaterial();
-    //material.vertexColors = THREE.VertexColors;
-    //geometry.faces[0].vertexColors.push(new THREE.Color(1,0,0));
-    //geometry.faces[0].vertexColors.push(new THREE.Color(0,1,0));
-    //geometry.faces[0].vertexColors.push(new THREE.Color(0,0,1));
-
-    //geometry.computeFaceNormals();
-    //material.side = THREE.DoubleSide();
-
-    var cube = new THREE.Mesh( geometry, material );
+    var cube = createCube(1);
+    cube.geometry.center();
 
     scene.add(cube);
 
     loop();
+
+    function loop()
+    {
+        requestAnimationFrame(loop);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+    }
 }
 
-function createCube(xs, ys, zs, len) {
+function createCube(len) {
   var vertices = [
-                     [xs, ys+len, zs]
-                     [xs, ys, zs],
-                     [xs+len, ys, zs],
-                     [xs+len, ys+len, zs],
+                     [0, len, 0],
+                     [0, 0, 0],
+                     [len, 0, 0],
+                     [len, len, 0],
 
-                     [xs, ys+len, zs + len]
-                     [xs, ys, zs + len],
-                     [xs+len, ys, zs + len],
-                     [xs+len, ys+len, zs + len]
+                     [0, len, -len],
+                     [0, 0, -len],
+                     [len, 0, -len],
+                     [len, len, -len]
                  ];
-  var faces = [[0, 1, 2],
-               [2, 3, 0],
+  var faces = [
+                [0, 1, 2],
+                [2, 3, 0],
 
-               [2, 3, 0],
-               [2, 3, 0],
+                [5, 4, 7],
+                [7, 6, 5],
 
-               [2, 3, 0],
-               [2, 3, 0],
+                [4, 0, 3],
+                [3, 7, 4],
 
-               [2, 3, 0],
-               [2, 3, 0],
+                [1, 5, 6],
+                [6, 2, 1],
 
-               [2, 3, 0],
-               [2, 3, 0],
+                [1, 0, 4],
+                [4, 5, 1],
 
-               [2, 3, 0],
-               [2, 3, 0]];
-
-  var v0 = new THREE.Vector3().fromArray(vertices[0]);
-  var v1 = new THREE.Vector3().fromArray(vertices[1]);
-  var v2 = new THREE.Vector3().fromArray(vertices[2]);
-  var v3 = new THREE.Vector3().fromArray(vertices[3]);
+                [3, 2, 6],
+                [6, 7, 3]
+             ];
 
   var geometry = new THREE.Geometry();
-  geometry.vertices.push(v0);
-  geometry.vertices.push(v1);
-  geometry.vertices.push(v2);
-  geometry.vertices.push(v3);
+  for (i=0; i<8; i++) {
+    var v = new THREE.Vector3().fromArray(vertices[i]);
+    geometry.vertices.push(v);
+  }
 
-  faces.forEach(function(id) {
+  for (i=0; i<12; i++) {
+    var id = faces[i];
     var f = new THREE.Face3(id[0], id[1], id[2]);
+    f.color = new THREE.Color((i+1)/12, 0, 0);
     geometry.faces.push(f);
-  });
-}
+  }
 
-function loop()
-{
-    requestAnimationFrame(loop);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
+  var material = new THREE.MeshBasicMaterial();
+  material.vertexColors = THREE.FaceColors;
+
+  material.side = THREE.FrontSide;
+  var cube = new THREE.Mesh( geometry, material );
+
+  return cube;
 }
--- W05/task2.js
+++ W05/task2.js
@@ -1,5 +1,7 @@
 function main()
 {
+    document.addEventListener( 'mousedown', mouse_down_event );
+
     var width = 500;
     var height = 500;
 
@@ -10,25 +12,105 @@ function main()
     var near = 1;
     var far = 1000;
     var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-    camera.position.set( 0, 0, 5 );
+    camera.position.set( 0, 0, 10 );
     scene.add( camera );
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize( width, height );
     document.body.appendChild( renderer.domElement );
 
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    var cube = createCube(1);
+    cube.geometry.center();
+
+    scene.add(cube);
 
     loop();
 
     function loop()
     {
-        requestAnimationFrame( loop );
-        cube.rotation.x += 0.001;
-        cube.rotation.y += 0.001;
-        renderer.render( scene, camera );
+        requestAnimationFrame(loop);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
     }
+
+    function mouse_down_event( event )
+    {
+      var x_win = event.clientX;
+      var y_win = event.clientY;
+
+      var vx = renderer.domElement.offsetLeft;
+      var vy = renderer.domElement.offsetTop;
+      var vw = renderer.domElement.width;
+      var vh = renderer.domElement.height;
+      var x_NDC = 2 * (x_win - vx) / vw - 1;
+      var y_NDC = -(2 * (y_win - vy) / vh - 1);
+
+      var p_NDC = new THREE.Vector3(x_NDC, y_NDC, 1);
+      var p_wld = p_NDC.unproject(camera);
+
+      var origin = camera.position;
+      var direction = p_NDC.sub(camera.position).normalize();
+
+      var raycaster = new THREE.Raycaster(origin, direction);
+      var intersects = raycaster.intersectObject(cube);
+      if (intersects.length > 0) {
+        intersects[0].face.color.setHex(Math.random() * 0xffffff);
+        intersects[0].object.geometry.colorsNeedUpdate = true;
+      }
+    }
+}
+
+function createCube(len) {
+  var vertices = [
+                     [0, len, 0],
+                     [0, 0, 0],
+                     [len, 0, 0],
+                     [len, len, 0],
+
+                     [0, len, -len],
+                     [0, 0, -len],
+                     [len, 0, -len],
+                     [len, len, -len]
+                 ];
+  var faces = [
+                [0, 1, 2],
+                [2, 3, 0],
+
+                [5, 4, 7],
+                [7, 6, 5],
+
+                [4, 0, 3],
+                [3, 7, 4],
+
+                [1, 5, 6],
+                [6, 2, 1],
+
+                [1, 0, 4],
+                [4, 5, 1],
+
+                [3, 2, 6],
+                [6, 7, 3]
+             ];
+
+  var geometry = new THREE.Geometry();
+  for (i=0; i<8; i++) {
+    var v = new THREE.Vector3().fromArray(vertices[i]);
+    geometry.vertices.push(v);
+  }
+
+  for (i=0; i<12; i++) {
+    var id = faces[i];
+    var f = new THREE.Face3(id[0], id[1], id[2]);
+    f.color = new THREE.Color((i+1)/12, 0, 0);
+    geometry.faces.push(f);
+  }
+
+  var material = new THREE.MeshBasicMaterial();
+  material.vertexColors = THREE.FaceColors;
+
+  material.side = THREE.FrontSide;
+  var cube = new THREE.Mesh( geometry, material );
+
+  return cube;
 }
